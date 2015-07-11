c77b267def693c583adc98488064f50f52c5f69c
Peter Georgeson <peter@supernifty.com.au>
2015-07-11 15:46:30 +1000

    support k-mers

--- dotplot/plot.py
+++ dotplot/plot.py
@@ -7,35 +7,50 @@ import Image
 import re
 import sys
 
-def dot(p, x, y):
+MAX_SIZE=4096
+
+def dot(p, x, y, size, max_size):
+  if size > max_size:
+    x = x * max_size / size
+    y = y * max_size / size
   p[x, y] = (0, 0, 0)
   
 def generate( reference_fh, candidate_fh=None, words=False, k=1 ):
   print "building k-mers..."
-  reference = collections.defaultdict(set)
+  reference = [] # pos -> kmers
+  history = [''] * k # for k-mer
+  candidate = collections.defaultdict(set) # kmers -> pos
   pos = 0
   for line in reference_fh:
-    line = line.strip()
+    line = line.strip().lower()
     if words:
       word_list = re.sub( "[^\w]", " ", line ).split()
       for word in word_list:
-        reference[word].add( pos )
+        history.insert( 0, word ) # insert at start
+        history.pop() # takes off end
+        identity = hash( ' '.join( history ) )
+        reference.append( identity )
+        candidate[identity].add( pos )
         pos += 1
     else:
-      for c in line:
-        reference[c].add( pos )
+      for word in line:
+        history.insert( 0, word ) # insert at start
+        history.pop() # takes off end
+        identity = hash( ''.join( history ) )
+        reference.append( identity )
+        candidate[identity].add( pos )
         pos += 1
   print "building k-mers: %i positions" % pos
 
   print "creating image"
-  image = Image.new( 'RGB', (pos, pos), "white" )
+  image = Image.new( 'RGB', (min(pos, MAX_SIZE), min(pos, MAX_SIZE)), "white" )
   pixels = image.load()
 
   print "generating dot plot"
-  for kmer in reference:
-    for x in reference[kmer]:
-      for y in reference[kmer]:
-        dot(pixels, x, y)
+  for x in xrange(pos):
+    identity = reference[x]
+    for y in candidate[identity]:
+        dot(pixels, x, y, pos, MAX_SIZE)
 
   print "showing image"
   image.show()
@@ -43,7 +58,7 @@ def generate( reference_fh, candidate_fh=None, words=False, k=1 ):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Generate dotplot from text')
   parser.add_argument('-k', dest='k', type=int, default=1, help='window size')
-  parser.add_argument('-w', dest='w', type=bool, default=False, help='word as unit')
+  parser.add_argument('-w', dest='w', type=bool, default=False, help='word as unit (default char)')
   #parser.add_argument('--reference', dest='reference', type=int, default=0, help='only consider alignments with at least this distance from the true location')
   args = parser.parse_args()
   print "Words is %s, %s k-mer size" % (args.w, args.k)
