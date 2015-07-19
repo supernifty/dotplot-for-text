77951327c6909c6a5e6f88554dc85ff86b73600e
Peter Georgeson <peter@supernifty.com.au>
2015-07-19 14:37:07 +1000

    support reference compare

--- README.md
+++ README.md
@@ -5,15 +5,28 @@ Generate dot plots using arbitrary text input.
 
 Background
 ----------
-Dot plots are a visualization tool often used in bioinformatics to compare to genomic sequences.
+Dot plots are a visualization tool often used in bioinformatics to compare genomic sequences.
 In particular, this technique is useful for finding repeated or nearly repeated subsequences.
 
 Similarly, this can be applied to arbitrary text to find similar pieces of text in two documents.
 
 Usage
 -----
+python plot.py [options] < filename
+
+Options
+-------
+-k: k-mer size to use
+-w: use True to compare using words instead of characters
+-m: max image size
+-r: reference to compare against (if not specified, file is compared against itself)
+
+Examples
+--------
+python plot.py < sample.txt
+python plot.py -k 4 -w True -r reference.txt < sample.txt
 
 TODO
 ----
-- compare different texts
 - annotate long repeats
+- tests
--- dotplot/plot.py
+++ dotplot/plot.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 # make a dot plot of incoming text
 
 import argparse
@@ -7,19 +7,20 @@ import Image
 import re
 import sys
 
-def dot(p, x, y, size, max_size):
-  if size > max_size:
-    x = x * max_size / size
-    y = y * max_size / size
+def dot(p, x, y, x_size, y_size, max_size):
+  if x_size > max_size:
+    x = x * max_size / x_size
+  if y_size > max_size:
+    y = y * max_size / y_size
   p[x, y] = (0, 0, 0)
   
-def generate( reference_fh, candidate_fh=None, words=False, k=1, max_size=1024 ):
-  print "building k-mers..."
-  reference = [] # pos -> kmers
+def generate( candidate_fh, reference_fh=None, words=False, k=1, max_size=1024 ):
+  print "building candidate k-mers..."
+  cand_map = [] # pos -> kmers
   history = [''] * k # for k-mer
-  candidate = collections.defaultdict(set) # kmers -> pos
-  pos = 0
-  for line in reference_fh:
+  ref_kmers = collections.defaultdict(set) # kmers -> pos
+  cand_pos = 0
+  for line in candidate_fh:
     line = line.strip().lower()
     if words:
       word_list = re.sub( "[^\w]", " ", line ).split()
@@ -27,28 +28,55 @@ def generate( reference_fh, candidate_fh=None, words=False, k=1, max_size=1024 )
         history.insert( 0, word ) # insert at start
         history.pop() # takes off end
         identity = hash( ' '.join( history ) )
-        reference.append( identity )
-        candidate[identity].add( pos )
-        pos += 1
+        cand_map.append( identity )
+        if reference_fh is None:
+          ref_kmers[identity].add( cand_pos )
+        cand_pos += 1
     else:
       for word in line:
         history.insert( 0, word ) # insert at start
         history.pop() # takes off end
         identity = hash( ''.join( history ) )
-        reference.append( identity )
-        candidate[identity].add( pos )
-        pos += 1
-  print "building k-mers: %i positions" % pos
+        cand_map.append( identity )
+        if reference_fh is None:
+          ref_kmers[identity].add( cand_pos )
+        cand_pos += 1
+  print "building candidate k-mers: %i positions" % cand_pos
 
+  if reference_fh is not None:
+    print "building reference k-mers..."
+    ref_pos = 0
+    for line in reference_fh:
+      line = line.strip().lower()
+      if words:
+        word_list = re.sub( "[^\w]", " ", line ).split()
+        for word in word_list:
+          history.insert( 0, word ) # insert at start
+          history.pop() # takes off end
+          identity = hash( ' '.join( history ) )
+          ref_kmers[identity].add( ref_pos )
+          ref_pos += 1
+      else:
+        for word in line:
+          history.insert( 0, word ) # insert at start
+          history.pop() # takes off end
+          identity = hash( ''.join( history ) )
+          cand_map.append( identity )
+          ref_kmers[identity].add( ref_pos )
+          ref_pos += 1
+    print "building reference k-mers: %i positions" % ref_pos
+  else:
+    ref_pos = cand_pos
+ 
   print "creating image"
-  image = Image.new( 'RGB', (min(pos, max_size), min(pos, max_size)), "white" )
+  image = Image.new( 'RGB', (min(cand_pos, max_size), min(ref_pos, max_size)), "white" )
   pixels = image.load()
 
   print "generating dot plot"
-  for x in xrange(pos):
-    identity = reference[x]
-    for y in candidate[identity]:
-        dot(pixels, x, y, pos, max_size)
+  for x in xrange(cand_pos):
+    identity = cand_map[x]
+    for y in ref_kmers[identity]:
+        dot(pixels, x, y, cand_pos, ref_pos, max_size)
 
   print "showing image"
   image.show()
@@ -58,7 +86,10 @@ if __name__ == '__main__':
   parser.add_argument('-k', dest='k', type=int, default=1, help='window size (default 1)')
   parser.add_argument('-w', dest='w', type=bool, default=False, help='word as unit (default char)')
   parser.add_argument('-m', dest='m', type=int, default=1024, help='max image size (default 1024)')
-  #parser.add_argument('--reference', dest='reference', type=int, default=0, help='only consider alignments with at least this distance from the true location')
+  parser.add_argument('-r', dest='reference', required=False, help='compare stdin to this file')
   args = parser.parse_args()
   print "Words is %s, %s k-mer size" % (args.w, args.k)
-  generate( sys.stdin, candidate_fh=None, words=args.w, k=args.k, max_size=args.m )
+  if args.reference is None:
+    generate( sys.stdin, reference_fh=None, words=args.w, k=args.k, max_size=args.m )
+  else:
+    generate( sys.stdin, reference_fh=open( args.reference, 'r' ), words=args.w, k=args.k, max_size=args.m )
